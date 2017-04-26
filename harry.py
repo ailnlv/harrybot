71d43c68625258581753a9d781618bf0afd5a825
ailnlv <ailnlv@users.noreply.github.com>
2017-04-26 11:50:44 -0300

    Create harry.py
    Harry responde con una chuchada al azar

---
+++ harry.py
@@ -0,0 +1,22 @@
+import random
+import requests
+
+token = "376658113:AAHh2TcsvcuKAFwYuBeQwCfSLUBUQm2Dfms"
+api_url = "https://api.telegram.org/bot{}/sendMessage".format(token)
+mensajes = [
+    "chupa el pico",
+    "aweonao",
+    "ojala te mueras",
+    "jan culiao te amo",
+  ]
+
+s = random.choice(mensajes)
+print s
+
+if 'message' not in Hook['params']:
+    Hook['params']['message']=dict(chat=dict(id=12700726))
+
+requests.get(api_url, params={
+    'chat_id': Hook['params']['message']['chat']['id'],
+    'text': s,
+}, verify=False)
