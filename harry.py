f52e614d29adceee19026b7dbc36850ee0297284
Andres Letelier <letelier@gmail.com>
2017-04-27 11:06:39 -0300

    mensajes al azar con markovgen

--- harry.py
+++ harry.py
@@ -1,21 +1,27 @@
 import random
 import requests
+from markovgen import Markov
+
+with open('corpus.txt') as corpus:
+    m = Markov(corpus)
+
 
 token = "376658113:AAHh2TcsvcuKAFwYuBeQwCfSLUBUQm2Dfms"
 api_url = "https://api.telegram.org/bot{}/sendMessage".format(token)
-mensajes = [
-    "chupa el pico",
-    "aweonao",
-    "ojala te mueras",
-    "jan culiao te amo",
-  ]
+# mensajes = [
+#     "chupa el pico",
+#     "aweonao",
+#     "ojala te mueras",
+#     "jan culiao te amo",
+# ]
 
-s = random.choice(mensajes)
-
+# s = random.choice(mensajes)
+s = m.generate_markov_text()
+print s
 if 'message' not in Hook['params']:
-    Hook['params']['message']=dict(chat=dict(id=12700726), text='/harry')
+    Hook['params']['message'] = dict(chat=dict(id=12700726), text='/harry')
 
-if '/harry' in Hook['params']['message']['text']:    
+if '/harry' in Hook['params']['message']['text']:
     requests.get(api_url, params={
         'chat_id': Hook['params']['message']['chat']['id'],
         'text': s,
