b587cf1445a3132db39fcbf8522f568b41d6f1b8
Andres Letelier <letelier@gmail.com>
2017-05-07 12:18:14 -0300

    hello world ahora devuelve un harry random

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from markovgen import Markov
+import requests
+
+with open('corpus.txt') as corpus:
+    m = Markov(corpus)
+
 app = Flask(__name__)
+
+token = "376658113:AAHh2TcsvcuKAFwYuBeQwCfSLUBUQm2Dfms"
+api_url = "https://api.telegram.org/bot{}/sendMessage".format(token)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return m.generate_markov_text()
 
 if __name__ == "__main__":
     app.run()
