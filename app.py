459a43e7578ed0c43e5b0f3bf1080a070e6f29b9
Andres Letelier <letelier@gmail.com>
2018-12-19 01:26:42 -0300

    harrybot respondiendo ecos

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ sal = [
     "el pene",
     "chilezuela",
     "la concerta",
-    "anos"
+    "anos",
 ]
 
 
@@ -33,13 +33,19 @@ def harry():
         return m.generate_markov_text()
     params = dict()
     data = flask.request.get_json()
-    message = data['message']
-    if '/harry' in message["text"]:
+    try:
+        message = data['message']
         params = dict(
             chat_id=message['chat']['id'],
-            text="jan culiao chupa " + random.choice(sal),
         )
+        if '/harry' in message["text"]:
+            params['text'] = "jan culiao chupa " + random.choice(sal),
+        elif '/echo' in message["text"]:
+            params['text'] = json.dumps(message)
         requests.get(api_url, params=params)
+
+    except:
+        flask.abort(400)
     return json.dumps(params)
 
 if __name__ == "__main__":
